ca01042717c7c92b9f7704da2e1546b9a013a567
alewickiy <lewickiy.a@yandex.ru>
2023-10-13 20:44:10 +0300

    Initial commit

---
+++ src/test/java/org/lewickiy/relationships/RelationshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lewickiy.relationships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
